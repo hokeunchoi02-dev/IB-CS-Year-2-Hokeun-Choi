f238f1f5f680b6fe71cbbb66455bf643a1e59087
hokeunchoi02-dev <hokeunchoi02@gmail.com>
2026-08-20 00:51:48 +0700

    Fix random number generation and output method

--- ranNum.java
+++ ranNum.java
@@ -1,11 +1,11 @@
 public class ranNum {
   public String ranNum() {
-     int num = (int) Math.random() * (256 + 1);
+     int num = (int) (Math.random() * (256 + 1));
      return num + "";
   }
 
   public static void main(String[] args) {
     ranNum generator = new ranNum();
-    System.out.println(generator);
+    System.out.println(generator.ranNum());
   }
 }
