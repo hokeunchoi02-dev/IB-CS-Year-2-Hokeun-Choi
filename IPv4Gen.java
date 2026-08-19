5af11f37b00d183928d8550f57cdfa56304f33d9
hokeunchoi02-dev <hokeunchoi02@gmail.com>
2026-08-20 00:42:10 +0700

    task 2

--- IPv4Gen.java
+++ IPv4Gen.java
@@ -1,1 +1,13 @@
-
+public class IPv4Gen {
+  public String generate() {
+    int a = (int) (Math.random() * 256);
+    int b = (int) (Math.random() * 256);
+    int c = (int) (Math.random() * 256);
+    int d = (int) (Math.random() * 256);
+    return a + "." + b + "." + c + "." + d;
+  }
+  public static void main(String[] args) {
+    IPv4Gen addr = new IPv4Gen();
+    System.out.println(addr.generate);
+  }
+}
