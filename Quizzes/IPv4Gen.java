7cd31f447e240fb5dca678f0f94e548934b6bd40
hokeunchoi02-dev <hokeunchoi02@gmail.com>
2026-08-24 09:34:53 +0700

    Fix method call for generating IPv4 address

--- Quizzes/IPv4Gen.java
+++ Quizzes/IPv4Gen.java
@@ -7,7 +7,7 @@ public class IPv4Gen {
     return a + "." + b + "." + c + "." + d;
   }
   public static void main(String[] args) {
-    IPv4Gen addr = new IPv4Gen();
-    System.out.println(add.generate);
+    IPv4Gen add = new IPv4Gen();
+    System.out.println(add.generate());
   }
 }
